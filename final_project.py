89a16e003d0032963367409f5bf67e747c659552
B.Vamsi Krishna <bommisettyvamsikrishna04@gmail.com>
2019-02-25 13:41:09 +0530

    Update final_project.py
    Added New Comments

--- final_project.py
+++ final_project.py
@@ -2,20 +2,39 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Project, Property, User
+
+# Import Flask class from flask library
+# request to get form data
+# jsonify to configure endpoint for application
 from flask import Flask, render_template, request
 from flask import redirect, url_for, flash, jsonify
+# Works as dictionary To store values 
+# for longetivity of user's session with server
 from flask import session as login_session
+# Random , String To create pseudo random string
+# which will identify each session
 import random
 import string
+# flow_from_clientsecrets method creates flow object
+# from clientsecrets json file
+# which stores client id, client secret, and other OAuth parameters
 from oauth2client.client import flow_from_clientsecrets
+# FlowExchangeError to catch if any error occours when trying 2 exchange
+# authorization code for access token
 from oauth2client.client import FlowExchangeError
+# HTTP client library
 import httplib2
+# To convert in memory objects to json
 import json
+# make_response method used to convert returned value from function
+# into real response object we can send of to the client
 from flask import make_response
+# it is a HTTP library
 import requests
 
 
-# Initialise the Flask app object
+# create the instance of the Flask class
+# with the name of the running application as argument
 app = Flask(__name__)
 
 
@@ -27,11 +46,16 @@ session = scoped_session(DBSession)
 
 
 # To Show All The Projects
+# Below function gets executed when
+# user enter any of below 2 routes in browser
 @app.route('/')
 @app.route('/projects/')
 def showProjects():
+    # To read everything from table Project
     projects = session.query(Project).all()
     if 'username' not in login_session:
+        # Flask configures a template engine to store html code
+        # pass html file name and variable/keyword
         return render_template('publicProjects.html', projects=projects)
     else:
         return render_template('showProjects.html', projects=projects)
@@ -40,6 +64,7 @@ def showProjects():
 # To Create a New Project
 @app.route('/project/new', methods=['GET', 'POST'])
 def newProject():
+    # If user is not logged in redirect to login page
     if 'username' not in login_session:
         return redirect('/login')
     if request.method == 'POST':
@@ -55,6 +80,7 @@ def newProject():
 
 
 # To Edit a Project
+# We are adding variable to the url i.e project_id
 @app.route('/project/<int:project_id>/edit', methods=['GET', 'POST'])
 def editProject(project_id):
     editProjectName = session.query(Project).filter_by(
@@ -67,6 +93,7 @@ def editProject(project_id):
     if request.method == 'POST':
         if request.form['editedProjectName']:
             prev_name = editProjectName.project_name
+            # Update with user entered ProjectName
             editProjectName.project_name = request.form['editedProjectName']
         session.add(editProjectName)
         session.commit()
@@ -186,6 +213,7 @@ def deletePropertyItem(project_id, property_id):
     deletedItem = session.query(
         Property).filter_by(property_id=property_id).one()
     if request.method == 'POST':
+        # To delete the propertyItem
         session.delete(deletedItem)
         session.commit()
         flash("Property Item %s is Deleted! " % (deletedItem.property_name))
@@ -257,23 +285,35 @@ def showPayingGuestProjects():
 
 
 # Create anti-forgery state token
+# To check request is from the intended user not malicious script
 @app.route('/login')
 def showLogin():
     """Show the login screen to the user."""
+    # state variable with 32 char long with upper case and letters 
+    # which will be psaaing on to the client
+    # which changes on refresh
+    # Later we check for same value with user and login session 
+    # when user tries to authenticate
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
                     for x in xrange(32))
     login_session['state'] = state
+    # state token is send to the client
     return render_template('login.html', STATE=state)
 
 
 # To login using Facebook OAuth
 @app.route('/fbconnect', methods=['POST'])
 def fbconnect():
+    # Compare the state sent from client and in login session
+    # To protect from Anti Forgery attack
     if request.args.get('state') != login_session['state']:
         response = make_response(json.dumps('Invalid state parameter.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
+    
+    # Exchange short lived token for the Long lived token
     access_token = request.data
+    
     print "access token received %s " % access_token
 
     app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
@@ -355,18 +395,24 @@ def fbdisconnect():
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
     """Performs app login via Google oauth."""
-    # Validate state token
+    # Validate state token sent from client and in login session
     if request.args.get('state') != login_session['state']:
         response = make_response(json.dumps('Invalid state parameter.'), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
-    # Obtain authorization code
+    
+    # Obtain authorization code(One time code)
     code = request.data
 
     try:
-        # Upgrade the authorization code into a credentials object
+        # Upgrade the authorization code into get  a credentials object
+        # which contains access token for my server
+        # below line create Oauth flow object and add my clients secret key info into it
         oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        # One time code flow my server will be sending of
         oauth_flow.redirect_uri = 'postmessage'
+        # initiate step 2 exchange function of flow class to
+        # exchange authorization for credentials object from google
         credentials = oauth_flow.step2_exchange(code)
     except FlowExchangeError:
         response = make_response(
@@ -376,8 +422,10 @@ def gconnect():
 
     # Check that the access token is valid.
     access_token = credentials.access_token
+    # Google API server will validate it using below url
     url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
            % access_token)
+    # Create a JSON get request containing url and access token
     h = httplib2.Http()
     result = json.loads(h.request(url, 'GET')[1])
     # If there was an error in the access token info, abort.
@@ -387,13 +435,16 @@ def gconnect():
         return response
 
     # Verify that the access token is used for the intended user.
+    # Grab the id of the access token in credential object
     gplus_id = credentials.id_token['sub']
+    # Compare id in credentials object with id in id from Google API server
     if result['user_id'] != gplus_id:
         response = make_response(
             json.dumps("Token's user ID doesn't match given user ID."), 401)
         response.headers['Content-Type'] = 'application/json'
         return response
-
+    
+    # Get client id from client_secrets json file
     CLIENT_ID = json.loads(
         open('client_secrets.json', 'r').read())['web']['client_id']
     APPLICATION_NAME = "Properties Menu Application"
@@ -405,10 +456,12 @@ def gconnect():
         print "Token's client ID does not match app's."
         response.headers['Content-Type'] = 'application/json'
         return response
-
+     
+    # Check to see if user is already logged in
     stored_access_token = login_session.get('access_token')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_access_token is not None and gplus_id == stored_gplus_id:
+        # Successful authentication
         response = make_response(json.dumps(
             'Current user is already connected.'),
                                  200)
@@ -420,11 +473,13 @@ def gconnect():
     login_session['access_token'] = credentials.access_token
     login_session['gplus_id'] = gplus_id
 
-    # Get user info
+    # Get user info from google plus api
+    # Send message to Google api with my access token 
+    # Requesting user info allowed by my token scope and store in data
     userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
     params = {'access_token': credentials.access_token, 'alt': 'json'}
     answer = requests.get(userinfo_url, params=params)
-
+    
     data = answer.json()
 
     login_session['username'] = data['name']
@@ -435,6 +490,7 @@ def gconnect():
     user_id = getUserID(login_session['email'])
     if not user_id:
         user_id = createUser(login_session)
+    # store user id in login session
     login_session['user_id'] = user_id
 
     output = ''
@@ -450,18 +506,21 @@ def gconnect():
     return output
 
 
+# Take login session as input
 def createUser(login_session):
     """Create a new user in the database."""
     newUser = User(name=login_session['username'], email=login_session[
                    'email'], picture=login_session['picture'])
     session.add(newUser)
     session.commit()
+    # To get only one result from table user with property email
     user = session.query(User).filter_by(email=login_session['email']).one()
     return user.id
 
 
 # To get user object
 def getUserInfo(user_id):
+    # Filter User table with property id and get only one result
     user = session.query(User).filter_by(id=user_id).one()
     return user
 
@@ -475,10 +534,11 @@ def getUserID(email):
         return None
 
 
-# DISCONNECT - Revoke a current user's token and reset their login_session
+# DISCONNECT - Revoke/reject a current user's token and reset their login_session
 @app.route('/gdisconnect')
 def gdisconnect():
     access_token = login_session.get('access_token')
+    # We have no record of the user
     if access_token is None:
         response = make_response(
             json.dumps('Current user not connected.'), 401)
@@ -492,7 +552,8 @@ def gdisconnect():
     h = httplib2.Http()
     result = h.request(url, 'GET')[0]
     print result['status']
-
+    
+    # if 200 I had successfuly diconnected the user
     if result['status'] == '200':
         del login_session['access_token']
         del login_session['gplus_id']
@@ -556,7 +617,12 @@ def propertyItemJSON(project_id, property_id):
     return jsonify(PropertyItem=propertyItem.serialize)
 
 
+# special variable name gets name as main
+# if run by Python interpretor
 if __name__ == '__main__':
+    # Flask will use to create sessions for our users
     app.secret_key = 'super_secret_key'
+    # Server will load itself each time code is changed
     app.debug = True
+    # To run the local server with application
     app.run(host='0.0.0.0', port=5000)
